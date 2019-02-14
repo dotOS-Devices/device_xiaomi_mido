8fc9b3889bd11cba4abf7fc2e90d051f182f4632
Mohan Cm <mohanmanjappa@gmail.com>
2019-02-14 19:06:53 +0530

    Add Gcam as default build ver: GCam-5.1.018-Pixel2Mod-Arnova8G2-V8.3b1

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Snap
+    GoogleCameraMod
 
 # Configstore
 PRODUCT_PACKAGES += \
