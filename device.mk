450c49041e123d9b31a22404a944604580603ace
Mohan Cm <mohanmanjappa@gmail.com>
2019-05-30 14:48:38 +0000

    build snap Camera * removed GoogleModCam for now

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    GoogleCameraMod
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
