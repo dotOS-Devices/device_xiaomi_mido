5e3879ae5291499d3f22436e1c513f1daa6f5d2e
Mohan Cm <mohanmanjappa@gmail.com>
2020-12-18 18:17:59 +0000

    mido: DotOS BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/dot_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    dot_mido-user \
+    dot_mido-userdebug \
+    dot_mido-eng
